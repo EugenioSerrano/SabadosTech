5be55dbd21654844798d054bae7d3d211ccd7f96
juacax <mateos.joaquin@gmail.com>
2020-10-24 13:09:18 -0300

    .

--- NodeProjects/Mongoose/Hello.js
+++ NodeProjects/Mongoose/Hello.js
@@ -56,5 +56,3 @@ var data = [
         console.log(result);
     }
   });
-
-  
---
+++ ReactJsProjects/Contador/js/funciones.js
@@ -0,0 +1,18 @@
+class DivRoot extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { contador: 0 };
+  }
+
+  render() {
+        var contador = this.state.contador;
+        var label = React.createElement("label",{key:"label"},contador);
+        var button = React.createElement("button",{key:"button", onClick: () => this.setState({contador:contador+1})},"Aumentar");
+        var br = React.createElement("br",{key:"br"});
+        var div = React.createElement("div",null,[label,br,button]);
+        return div;
+    }
+}
+
+const domContainer = document.querySelector('#rootDiv');
+ReactDOM.render(React.createElement(DivRoot), domContainer);
