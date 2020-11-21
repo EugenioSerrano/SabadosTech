8ff0efbac1edbc165e057eccfc4a5281cf20002d
juacax <mateos.joaquin@gmail.com>
2020-11-21 12:35:52 -0300

    TaTeTi

--- ReactJsProjects/tateti/src/index.js
+++ ReactJsProjects/tateti/src/index.js
@@ -1,10 +1,62 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+class Board extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null),
+      SigueX: true
+    };
+  }
+
+  crearCuadrado(i) {
+    return <Square value={this.state.squares[i]} onClick={() => this.cambiarEstado(i)} />;
+  }
+  
+  cambiarEstado(i) {
+    const squares = this.state.squares.slice();
+    squares[i] = this.state.SigueX ? 'X' : 'O';
+    this.setState({squares: squares,SigueX: !this.state.SigueX});
+  }
+
+  render() {
+    const siguienteJugador = 'Siguiente: ' + (this.state.SigueX ? 'X' : 'O');
+    return (
+      <div>
+        <div className="status">{siguienteJugador}</div>
+        <div className="board-row">
+          {this.crearCuadrado(0)}
+          {this.crearCuadrado(1)}
+          {this.crearCuadrado(2)}
+        </div>
+        <div className="board-row">
+          {this.crearCuadrado(3)}
+          {this.crearCuadrado(4)}
+          {this.crearCuadrado(5)}
+        </div>
+        <div className="board-row">
+          {this.crearCuadrado(6)}
+          {this.crearCuadrado(7)}
+          {this.crearCuadrado(8)}
+        </div>
+      </div>
+    );
+  }
+}
+
+function Square(props) {
+  return (
+    <button className="square" onClick={props.onClick}>
+      {props.value}
+    </button>
+  );
+}
 
 ReactDOM.render(
   <div>
-    <h1>HOLA </h1><h1>TATETI</h1>
+    <Board/>
   </div>,
   document.getElementById('root')
 );
+
