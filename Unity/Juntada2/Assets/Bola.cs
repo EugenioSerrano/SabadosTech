74164f94ec209ee1948b032bc123bda615ac901b
Eugenio Serrano <eugenioserrano@msn.com>
2021-01-09 11:21:09 -0300

    Acomodamos el repo para los proyectos de Unity

---
+++ Unity/Juntada2/Assets/Bola.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bola : MonoBehaviour {
+
+	int VelocidadX;
+	int VelocidadY;
+
+	public float VelocidadGeneral;
+
+	// Use this for initialization
+	void Start () {
+		VelocidadX = Random.Range (0, 2);
+		if (VelocidadX == 0) {
+			VelocidadX = 1;
+		} else {
+			VelocidadX = -1;
+		}
+
+		VelocidadY = Random.Range (0, 2);
+		if (VelocidadY == 0) {
+			VelocidadY = 1;
+		} else {
+			VelocidadY = -1;
+		}
+
+		GetComponent<Rigidbody>().velocity = new Vector3(VelocidadGeneral*VelocidadX, VelocidadGeneral*VelocidadY, 0);
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
