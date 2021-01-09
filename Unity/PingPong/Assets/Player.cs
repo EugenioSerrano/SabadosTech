51b7c0121ee757bb2037a2932d3da379b066e2b2
Eugenio Serrano <eugenioserrano@msn.com>
2021-01-09 12:33:24 -0300

    Acomodamos la velocidad de las raquetas

--- Unity/PingPong/Assets/Player.cs
+++ Unity/PingPong/Assets/Player.cs
@@ -1,25 +1,34 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System;
 using UnityEngine;
 
 public class Player : MonoBehaviour {
-	public KeyCode Arriba;
-	public KeyCode Abajo;
+ 
+	public KeyCode KeyUp;
+	public KeyCode KeyDown;
 
-	public float Velocidad; 
+	public float Speed; 
+
+
+	private DateTime _lastKeyRefresh;
+
 	// Use this for initialization
 	void Start () {
-		
+		_lastKeyRefresh = DateTime.Now.AddSeconds(-10);		
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
-		if (Input.GetKey (Arriba) && transform.position.y <= 4) {
-			transform.Translate (0, Velocidad, 0);
-		}
-		if (Input.GetKey (Abajo) && transform.position.y >= -4) {
-			transform.Translate (0, Velocidad*-1, 0);
+		if ((DateTime.Now - _lastKeyRefresh).Milliseconds >= 11 - Speed){
+			_lastKeyRefresh = DateTime.Now;
+			if (Input.GetKey (KeyUp) && transform.position.y <= 4) {
+				transform.Translate (0, 0.1f, 0);
+			}
+			if (Input.GetKey (KeyDown) && transform.position.y >= -4) {
+				transform.Translate (0, -0.1f, 0);
+			}
 		}
 	}
 }
