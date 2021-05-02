933e408f6163761ea4a30545b3b2c7c38fa45005
Eugenio Serrano <eugenioserrano@msn.com>
2021-05-01 20:47:25 -0400

    Toque desde debian

--- Svelte/HolaMundo/src/App.svelte
+++ Svelte/HolaMundo/src/App.svelte
@@ -5,6 +5,8 @@
 <main>
 	<h1>Hello {name}!</h1>
 	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+
+	<p>Esto es una prueba</p>
 </main>
 
 <style>
