dba5a87356f64efc842405e7e9726eb11cbbcd3b
Eugenio Serrano <eugenioserrano@msn.com>
2021-05-22 21:13:29 -0400

    cambiamos el directorio a hola-mundo

--- Rust/hola-mundo/src/main.rs
+++ Rust/hola-mundo/src/main.rs
@@ -10,5 +10,5 @@ fn main() {
     // Statements here are executed when the compiled binary is called
 
     // Print text to the console
-    println!("Hello World!");
+    println!("Hola Mundo!");
 }
